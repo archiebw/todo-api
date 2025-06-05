92c425eef0ffbf9befcac2ff1dc3eb34f8fbdc9e
Archie Brand-Williamson <abrandwilliamson@gmail.com>
2025-06-05 20:20:40 +0100

    feat: helm deployment for todo api

--- deploy/terraform/gke.tf
+++ deploy/terraform/gke.tf
@@ -77,7 +77,7 @@ module "todo_workload_identity" {
   version = "~> 36.0"
 
   name       = "id-${module.gke.name}"
-  namespace  = "todo"
+  namespace  = kubernetes_namespace.todo.name
   project_id = var.project_id
   roles      = ["roles/datastore.user"]
 }
